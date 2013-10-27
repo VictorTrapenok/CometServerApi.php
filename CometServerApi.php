d6320f0a8c1612ac9db9784b8b0dcbd830f1588a
Levhav <Levhav@gmail.com>
2013-10-28 01:36:10 +1100

    v1.12

--- CometServerApi.php
+++ CometServerApi.php
@@ -1,23 +1,22 @@
 <?php
-  
 
-define('ADD_USER_HASH', 1);
-     
-define('SEND_BY_USER_ID', 3); 
-define('SEND_GET_LAST_ONLINE_TIME', 5);
-define('SEND_EVENT', 6); 
- 
+/**
+ *  Comet Server PHP Библиотека
+ *  Библиотека предоставляет простое API для работы с Comet-Server.ru
+ *
+ *  Copyright 2013, Trapenok Victor. Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
+*/
 
 define('NO_ERROR', 0);
 define('ERROR_USER_OFLINE', -5);
 define('ERROR_USER_OFLINE_AND_OVERFLOW_QUEUE', -10);
-define('ERROR_USER_OFLINE_MORE_MAX_OFLINE_TIME', -11); 
+define('ERROR_USER_OFLINE_MORE_MAX_OFLINE_TIME', -11);
 
 define('ERROR_USER_CONECTION', -6);
 
 define('ERROR_UNDEFINED_EVENT', -7);
 define('ERROR_UNDEFINED_SERVER_EVENT', -1);
- 
+
 
 /**
  * Обращение с номером больше чем размер индекса подключёных пользователей
@@ -29,80 +28,150 @@ define('ERROR_MORE_MAX_USER_ID', -8);
  */
 define('ERROR_MORE_MAX_CONECTIONS', -9);
 
+/**
+ *  Comet Server PHP Библиотека
+ *  Библиотека предоставляет простое API для работы с Comet-Server.ru
+ *
+ *  $comet = CometServerApi::getInstance();
+ *  $comet->authorization(1, "0000000000000000000000000000000000000000000000000000000000000000");
+ *  $comet->send_event('my_event', Array("data" => "testing") );
+ *  $comet->send_event('my_event', "моё сообщение" );
+ *
+ *  Copyright 2013, Trapenok Victor. Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
+*/
 class CometServerApi
 {
-   var $version=0.8;
- 
-   static public $server = "comet-server.ru";
-   static public $port = 55552;
-   static public $timeOut = 1;
-   
-   static public $dev_id = 1;
-   static public $dev_key = "0000000000000000000000000000000000000000000000000000000000000000";
-   
-   static private $authorization = false;
-   
-   
-   static public $handle = false;
-   
-   
+   var $version=1.12;
+
+   protected $server = "comet-server.ru";
+   protected $port = 808;
+   protected $timeOut = 1;
+
+   protected $authorization = false;
+   protected $handle = false;
+
+   protected $dev_id = false;
+   protected $dev_key = false;
+
+   protected static $_instance;
+
+
+   protected static $ADD_USER_HASH = 1;
+   protected static $SEND_BY_USER_ID = 3;
+   protected static $SEND_GET_LAST_ONLINE_TIME = 5;
+   protected static $SEND_EVENT = 6;
+
+
+   /**
+    * Конструетор оставлен публичным на тот случай если вам реально понадобится использовать два соединения с комет сервером единовременно но с разными $dev_id и $dev_key
+    * Во всех остальных случаях используйте клас как singleton, тоесть вызывая CometServerApi::getInstance()
+    *
+    * @param int $dev_id Идентификатор разработчика
+    * @param string $dev_key Секретный ключ разработчика
+    */
+   public function __construct($dev_id = false, $dev_key = false)
+   {
+       if($dev_id !== false) $this->dev_id = $dev_id;
+       if($dev_key !== false) $this->dev_key = $dev_key;
+   }
+
+   /**
+    * @return CometServerApi
+    */
+   public static function getInstance()
+   {
+        if (null === self::$_instance) {
+            self::$_instance = new self();
+        }
+
+        return self::$_instance;
+    }
+
+   /**
+    * Позволяет указать $dev_id и $dev_key, необходимо вызвать один раз перед использованием.
+    *
+    * Данные для авторизации будут отправлены вместе с первым запросом  к комет серверу.
+    * Эта функция отдельного запроса не зоздаёт
+    *
+    * @param int $dev_id Идентификатор разработчика
+    * @param string $dev_key Секретный ключ разработчика
+    * @return \CometServerApi
+    */
+   public function authorization($dev_id, $dev_key)
+   {
+       $this->dev_id = $dev_id;
+       $this->dev_key = $dev_key;
+       return $this;
+   }
+
    /**
     * Выполняет запросы
     * @todo Можно перевести на систему обмена без закрытия соединения после каждого запроса
     * @param string $msg
-    * @return array 
+    * @return array
     */
-   static private function send($msg)
-   {       
-       if(!self::$handle)
+   private function send($msg, $isRetry = false)
+   {
+       if($this->dev_id === false || $this->dev_key === false)
        {
-            if(self::$timeOut)
+           return Array("error" => 1, "info" => "Не установлен dev_id или dev_key, перед использованием следует вызвать функцию authorization", "conection" => 0, "event" => ERROR_UNDEFINED_EVENT);
+       }
+
+       if(!$this->handle)
+       {
+            if($this->timeOut)
             {
-                self::$handle = @fsockopen("d".self::$dev_id.".app.".self::$server, self::$port,$e1,$e2,self::$timeOut);
+                $this->handle = @fsockopen("d".$this->dev_id.".app.".$this->server, $this->port,$e1,$e2,$this->timeOut);
             }
             else
             {
-                self::$handle = @fsockopen(self::$server, self::$port);
+                $this->handle = @fsockopen($this->server, $this->port);
             }
        }
-       
-       if(self::$handle)
+
+       if($this->handle)
        {
-           if(!self::$authorization)
-           { 
-               $msg = "A:::".self::$dev_id.";".self::$dev_key.";".$msg;
-               self::$authorization = true;
+           if(!$this->authorization)
+           {
+               $msg = "A:::".$this->dev_id.";".$this->dev_key.";".$msg;
+               $this->authorization = true;
            }
-           
-           fputs(self::$handle, $msg, strlen($msg) );
-           
-           $tmp = fgets(self::$handle);    
+
+
+           if( @fputs($this->handle, $msg, strlen($msg) ) === false)
+           {
+               $this->handle = false;
+               if($isRetry) return $this->send($msg, true);
+           }
+
+           $tmp = fgets($this->handle);
+           //echo  "[".$tmp."]\n" ;
            return json_decode($tmp,true);
        }
-       return false; 
+       return false;
    }
-   
-   static public function add_user_hash($user_id, $hash = false)
+
+   public function add_user_hash($user_id, $hash = false)
    {
       if($hash === false)
       {
           $hash = session_id();
       }
-      
-      return self::send(ADD_USER_HASH.";".$user_id.";".session_id()."");
+
+      return $this->send(self::$ADD_USER_HASH.";".$user_id.";".session_id()."");
    }
-    
+
     /**
      * Отправка сообщения списку пользователей
      * @param array $user_id_array Масив с идентификаторами получателей
      * @param array $msg Структура данных которая будет отправлена, должна содержать поле name с именем адресата (chat, и др.)
-     * @return array 
-     * 
+     * @return array
+     *
      * Перед отправкой данные конвертируются в json а затем кодируются в base64
      * Структура данных которая будет отправлена принимающий js ожидает увидеть поле name - имя события
      */
-   static public function send_by_user_id($user_id_array,$msg)
-   {  
+   public function send_by_user_id($user_id_array,$msg)
+   {
         if(!is_array($user_id_array))
         {
             if( (int)$user_id_array > 0)
@@ -110,7 +179,7 @@ class CometServerApi
                 $n = 1;
             }
             else
-            { 
+            {
                 return false;
             }
         }
@@ -127,39 +196,39 @@ class CometServerApi
             }
             $user_id_array = implode(";",$user_id_array);
         }
-        
+
         if(!is_string($msg))
         {
             $msg = json_encode($msg);
         }
-        
+
       $msg = base64_encode($msg)."";
-      return self::send(SEND_BY_USER_ID.";".$n.";".$user_id_array.";".$msg);
+      return $this->send(self::$SEND_BY_USER_ID.";".$n.";".$user_id_array.";".$msg);
    }
-    
+
    /**
     * Отправка сообщения списку пользователей
     * @param type $user_id_array
     * @param type $event_name
     * @param type $msg
     */
-   static public function send_to_user($user_id_array, $event_name, $msg)
+   public function send_to_user($user_id_array, $event_name, $msg)
    {
-       self::send_by_user_id($user_id_array,Array("text"=>$msg,"event_name"=>$event_name) );
+       return $this->send_by_user_id($user_id_array,Array("data"=>$msg,"event_name"=>$event_name) );
    }
-   
+
      /**
       * Определяет количество секунд прошедшее с момента последнего прибывания пользователя online
       * Пример ответа:
       * {"conection":1,"event":5,"error":0,"answer":"0"}
-      * 
+      *
       * Если answer = 0 то человек online
-      * 
+      *
       * @param int $user_id
-      * @return array 
+      * @return array
       */
-   static public function get_last_online_time($user_id_array)
-   { 
+   public function get_last_online_time($user_id_array)
+   {
         if(!is_array($user_id_array))
         {
             $n = 1;
@@ -168,44 +237,42 @@ class CometServerApi
         {
             $n = count($user_id_array);
             $user_id_array = implode(";",$user_id_array);
-        } 
-      return self::send(SEND_GET_LAST_ONLINE_TIME.";".$n.";".$user_id_array.";");
+        }
+      return $this->send(self::$SEND_GET_LAST_ONLINE_TIME.";".$n.";".$user_id_array.";");
    }
-    
+
     /**
      * Отправляет произвольное сообщение серверу
      * @param int $event_id
      * @param string $msg
      * @param bool $isCoding если true то сообщение перед отправкой будет закодировано в base64
-     * @return array 
+     * @return array
      */
-   static public function send_event($event,$msg, $isCoding = false)
+   public function send_event($event,$msg, $isCoding = false)
    {
-      if(!is_string($msg))
-      {
-          $msg = json_encode($msg);
-      }
-      
       if($isCoding == false)
       {
+      	  if(!is_string($msg))
+          {
+          	  $msg = json_encode($msg);
+          }
           $msg = base64_encode($msg);
       }
-      
+
       $n = 1;
       if( is_array($event) )
       {
-          $answer = Array(); 
+          $answer = Array();
           foreach ($event as $key => $value)
           {
-              $answer[] = self::send(SEND_EVENT.";".$value.";".$msg);
+              $answer[] = $this->send(self::$SEND_EVENT.";".$value.";".$msg);
           }
-          
+
           return $answer;
       }
-      
-      
-      return self::send(SEND_EVENT.";".$event.";".$msg);
+
+      return $this->send(self::$SEND_EVENT.";".$event.";".$msg);
    }
-       
+
 }
 ?>
