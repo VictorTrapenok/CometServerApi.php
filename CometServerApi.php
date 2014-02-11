36e015df7f9cab44bfa56b85c104741dbc60d398
Levhav <Levhav@yandex.ru>
2014-02-11 20:46:41 +1100

    Новые возможности

--- CometServerApi.php
+++ CometServerApi.php
@@ -9,7 +9,60 @@
 
 define('NO_ERROR', 0);  
 define('ERROR_UNDEFINED_EVENT', -7); 
+define('ERROR_CONNECTION', -100); 
  
+class comet_response{
+    
+    protected $data;
+    
+    public function __construct($data) {
+        $this->data = $data;
+    }
+     
+    public function getError()
+    {
+        if( isset($this->data['error']))
+        {
+            return $this->data['error'];
+        }
+
+        return false;
+    }
+    
+    public function getInfo()
+    {
+        if( isset($this->data['info']))
+        {
+            return $this->data['info'];
+        }
+
+        return false;
+    }
+    
+    public function getData($index = false)
+    {
+        if( isset($this->data['data']))
+        {
+            if( $index !== false)
+            {
+                if( isset($this->data['data'][$index]) )
+                {
+                    return $this->data['data'][$index];
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            
+            return $this->data['data'];
+        }
+
+        return false;
+    }
+}
+
+
 /**
  *  Comet Server PHP Библиотека
  *  Библиотека предоставляет простое API для работы с Comet-Server.ru
@@ -45,7 +98,12 @@ class CometServerApi
    protected static $SEND_GET_LAST_ONLINE_TIME = 5;
    protected static $SEND_EVENT = 6;
 
-
+   protected static $POP_UNDELIVERED_MESSAGES = 9;
+   protected static $CLEAR_UNDELIVERED_MESSAGES = 10;
+   protected static $COUNT_UNDELIVERED_MESSAGES = 11;
+   protected static $SET_PIPE_SETTINGS = 12;
+ 
+   
    /**
     * Конструетор оставлен публичным на тот случай если вам реально понадобится использовать два соединения с комет сервером единовременно но с разными $dev_id и $dev_key
     * Во всех остальных случаях используйте клас как singleton, тоесть вызывая CometServerApi::getInstance()
@@ -79,7 +137,7 @@ class CometServerApi
     *
     * @param int $dev_id Идентификатор разработчика
     * @param string $dev_key Секретный ключ разработчика
-    * @return \CometServerApi
+    * @return CometServerApi
     */
    public function authorization($dev_id, $dev_key)
    {
@@ -92,13 +150,13 @@ class CometServerApi
     * Выполняет запросы
     * @todo Можно перевести на систему обмена без закрытия соединения после каждого запроса
     * @param string $msg
-    * @return array
+    * @return comet_response
     */
    private function send($msg, $isRetry = false)
    {
        if($this->dev_id === false || $this->dev_key === false)
        {
-           return Array("error" => 1, "info" => "Не установлен dev_id или dev_key, перед использованием следует вызвать функцию authorization", "conection" => 0, "event" => ERROR_UNDEFINED_EVENT);
+           return new comet_response(Array("info" => "Не установлен dev_id или dev_key, перед использованием следует вызвать функцию authorization", "error" => ERROR_UNDEFINED_EVENT, "data" => ""));
        }
 
        if(!$this->handle)
@@ -117,7 +175,7 @@ class CometServerApi
        {
            if(!$this->authorization)
            {
-               $msg = "A:::".$this->dev_id.";".self::$major_version.";".self::$minor_version.";".$this->dev_key.";".$msg;
+               $msg = "A:::".$this->dev_id.";".self::$major_version.";".self::$minor_version.";".$this->dev_key.";".$msg."\t";
                $this->authorization = true;
            }
 
@@ -130,9 +188,10 @@ class CometServerApi
 
            $tmp = fgets($this->handle);
            // echo  "[".$tmp."]\n" ;
-           return json_decode($tmp,true);
+           return new comet_response(json_decode($tmp,true));
        }
-       return false;
+       
+       return new comet_response(Array("info" => "Не удалось создать соединение.", "error" => ERROR_CONNECTION, "data" => ""));
    }
 
    public function add_user_hash($user_id, $hash = false)
@@ -154,7 +213,6 @@ class CometServerApi
    public function send_to_user($user_id_array, $event_name, $msg)
    {
         $msg = Array("data"=>$msg,"event_name"=>$event_name);
-       
         if(!is_array($user_id_array))
         {
             if( (int)$user_id_array > 0)
@@ -183,16 +241,16 @@ class CometServerApi
         return $this->send(self::$SEND_BY_USER_ID.";".$n.";".$user_id_array.";".base64_encode(json_encode($msg)));
    }
 
-     /**
-      * Определяет количество секунд прошедшее с момента последнего прибывания пользователя online
-      * Пример ответа:
-      * {"conection":1,"event":5,"error":0,"answer":"0"}
-      *
-      * Если answer = 0 то человек online
-      *
-      * @param int $user_id
-      * @return array
-      */
+   /**
+    * Определяет количество секунд прошедшее с момента последнего прибывания пользователя online
+    * Пример ответа:
+    * {"conection":1,"event":5,"error":0,"answer":"0"}
+    *
+    * Если answer = 0 то человек online
+    *
+    * @param int $user_id
+    * @return array
+    */
    public function get_last_online_time($user_id_array)
    {
         if(!is_array($user_id_array))
@@ -207,19 +265,91 @@ class CometServerApi
       return $this->send(self::$SEND_GET_LAST_ONLINE_TIME.";".$n.";".$user_id_array.";");
    }
 
-    /**
-     * Отправляет произвольное сообщение серверу
-     * @param int $event_id
-     * @param string $msg
-     * @return array
-     */
+   /**
+    * Отправляет произвольное сообщение серверу
+    * @param int $event_id
+    * @param string $msg
+    * @return array
+    */
    public function send_to_pipe($pipe, $event_name, $msg)
-   {
-      $msg = Array("data"=>$msg,"event_name"=>$event_name); 
-      $msg = base64_encode(json_encode($msg));
-  
-      return $this->send(self::$SEND_EVENT.";".$pipe.";".$msg);
+   {  
+      return $this->send(self::$SEND_EVENT.";".$pipe.";".base64_encode(json_encode( Array("data"=>$msg,"event_name"=>$event_name) )));
    }
 
+   /**
+    * Устанавливает длину лога сообщений в канале.
+    * @param string $pipe
+    * @param int $log_len
+    * @return array
+    */
+   public function set_pipe_settings($pipe, $log_len)
+   {  
+      $type = '0';
+      return $this->send(self::$SET_PIPE_SETTINGS.";".$type.';'.((int)$log_len).';'.$pipe);
+   }
+   
+   /**
+    * Извлекает одно сообщение из очереди пользователя удалая его из очереди
+    * Формат: &event, &user_id, &type
+    *
+    * type - 1 Выборка с конца очереди (то что попало в очередь раньше других)
+    * type - 2 Выборка с начала очереди (то что попало в очередь позже других)
+    * 
+    */
+   protected function pop_undelivered_messages($user_id, $type)
+   {
+      return $this->send(self::$POP_UNDELIVERED_MESSAGES.";".$user_id.";".$type);
+   }
+   
+   /**
+    * Извлекает то сообщение которое попало в очередь раньше других и удаляет его из очереди
+    */
+   protected function pop_first_undelivered_messages($user_id)
+   {
+      return $this->pop_undelivered_messages($user_id, 1);
+   }
+   
+   /**
+    * Извлекает то сообщение которое попало в очередь позже других и удаляет его из очереди
+    */
+   protected function pop_last_undelivered_messages($user_id)
+   {
+      return $this->pop_undelivered_messages($user_id, 2);
+   }
+   
+   /**
+    * Очищает очередь недоставленных сообщений для массива пользователей
+    */
+   protected function clear_undelivered_messages($user_id_array)
+   {
+        if(!is_array($user_id_array))
+        {
+            $n = 1;
+        }
+        else
+        {
+            $n = count($user_id_array);
+            $user_id_array = implode(";",$user_id_array);
+        }
+      return $this->send(self::$CLEAR_UNDELIVERED_MESSAGES.";".$n.";".$user_id_array.";"); 
+   }
+   
+   /**
+    * Получает количество недоставленных сообщения для массива пользователей
+    */
+   protected function count_undelivered_messages($user_id_array)
+   {
+        if(!is_array($user_id_array))
+        {
+            $n = 1;
+        }
+        else
+        {
+            $n = count($user_id_array);
+            $user_id_array = implode(";",$user_id_array);
+        }
+      return $this->send(self::$COUNT_UNDELIVERED_MESSAGES.";".$n.";".$user_id_array.";"); 
+   }
+   
 }
-?>
+
